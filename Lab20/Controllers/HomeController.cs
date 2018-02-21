71bcd179afb81917ced00650ef62af4a128374f6
John <jjfloreno@gmail.com>
2018-02-20 23:26:54 -0500

    First commit

---
+++ Lab20/Controllers/HomeController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Lab20.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult Registration()
+        {
+            ViewBag.Message = "Please enter your information to register:";
+            return View();
+        }
+ 
+        public ActionResult RegisterUser(string FirstName, string Position, string Email)
+        {
+            ViewBag.Message = $"Thank you for resgistering, {FirstName}!\nA confirmation has been sent to {Email}.";
+
+            if(Position == "Forward")
+            {
+                ViewBag.YourPosition = $"Go get 'em, Gretzky!";
+            }
+            else if(Position == "Defense")
+            {
+                ViewBag.YourPosition = $"Number four, Bobby Orr!";
+            }
+            else
+            {
+                ViewBag.YourPosition = $"Brick wall!";
+            }
+
+            return View("NewUser");
+        }
+    }
+}
---
+++ Lab20/Scripts/Validation.js
@@ -0,0 +1,45 @@
+﻿$(function () {
+    $("#AddUser").validate({
+        rules: {
+            FirstName: {
+                required: true
+            },
+            LastName: {
+                required: true
+            },
+            Email: {
+                required: true,
+                email: true
+            },
+            PWord: {
+                required: true
+            },
+            PWordConfirm: {
+                required: true,
+                equalTo: "#PWord"
+            },
+            Position: {
+                required:true
+            },
+            Jersey: {
+                number: true //needs work - how to make it between 1-99?
+            }
+        },
+        messages: {
+            Email: {
+                required: 'Please enter an email address',
+                email: 'Please enter a <em>valid</em> email address.'
+            },
+            PWordConfirm: {
+                equalTo: 'Passwords do not match.'
+            },
+            Position: {
+                required: 'Please select your position.'
+            }
+        }
+    });
+});
+
+if (!$('input[name=Position]:checked').val()) {
+    alert: 'Please select a position';
+}
